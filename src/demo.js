30c061241d60303e2f1a314025380fe383166b9b
Slmn-khn <69108170+Slmn-khn@users.noreply.github.com>
2021-08-09 10:54:16 +0530

    Initial commit

---
+++ src/demo.js
@@ -0,0 +1,157 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import Button from "@material-ui/core/Button";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper
+  },
+  list: {
+    width: 350
+  },
+  fullList: {
+    width: "auto"
+  }
+}));
+
+export default function SwipeableTemporaryDrawer() {
+  const classes = useStyles();
+  const [close, setClose] = React.useState(false);
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const closeFunc = () => {
+    console.log(close);
+    if (close) {
+      return toggleDrawer("right", false);
+    }
+    return toggleDrawer("right", true);
+  };
+  const list = (anchor) => (
+    <div
+      role="presentation"
+      onClick={closeFunc()}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <div>
+        <button onClick={() => (close ? setClose(false) : setClose(true))}>
+          Click
+        </button>
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="simple tabs example"
+            >
+              <Tab label="Item One" {...a11yProps(0)} />
+              <Tab label="Item Two" {...a11yProps(1)} />
+              <Tab label="Item Three" {...a11yProps(2)} />
+            </Tabs>
+          </AppBar>
+          <TabPanel value={value} index={0}>
+            Item One
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            Item Two
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            Item Three
+          </TabPanel>
+        </div>
+        <div
+          style={{
+            width: "100%",
+            background: "green",
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            padding: "20px",
+            boxSizing: "border-box",
+            display: "flex",
+            justifyContent: "space-evenly"
+          }}
+        >
+          <button>Footer</button>
+          <button>Footer1</button>
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <React.Fragment>
+      <Button onClick={toggleDrawer("right", true)}>Right</Button>
+      <SwipeableDrawer
+        anchor={"right"}
+        open={state["right"]}
+        onClose={toggleDrawer("right", false)}
+        onOpen={toggleDrawer("right", true)}
+      >
+        {list("right")}
+      </SwipeableDrawer>
+    </React.Fragment>
+  );
+}
